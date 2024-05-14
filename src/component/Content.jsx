fe3bc2119ab15bfc4e5d9659cd5b63d1ab88ffcb
10-pro-overview <yashjha00000@gmail.com>
2024-05-14 14:44:47 +0530

    changes in content section

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -10,7 +10,8 @@ const Content = () => {
 
     useGSAP(() => {
         let tl2 = gsap.timeline();
-
+        
+        if(window.innerWidth>520){
         tl2.from('.services', {
             y: 30,
             opacity: 0,
@@ -96,7 +97,7 @@ const Content = () => {
                 },
                 'line4'
             );
-
+        }
         ScrollTrigger.create({
             trigger: '.section',
             start: 'top 100%',
@@ -117,8 +118,8 @@ const Content = () => {
             </div>
 
             <div className='section overflow-x-hidden'>
-                <div className=" min-h-[85vh] w-full py-7 px-[80px] flex justify-between flex-wrap contain  max-medium:flex-col  max-sm:px-3">
-                    <div className=" flex-shrink-0 h-[333px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line1 left mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2  max-sm:mb-9">
+                <div className=" min-h-[85vh] w-full py-7 px-[80px] flex justify-between flex-wrap contain  max-medium:flex-col  max-sm:px-3 ">
+                    <div className=" flex-shrink-0 h-[333px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line1 left mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2  max-sm:mb-9 max-sm:hover:transform max-sm:hover:scale-105 max-sm:duration-500">
                         <div className="elem-part1 h-full w-[50%] flex justify-between flex-col  ">
                             <h2 className=' bg-lime-300 w-[89%] text-[23px] font-semibold px-2 rounded-md max-lg:text-[20px]  max-sm:text-[17px]  max-small:text-[15px] max-xs:text-[13px] '>Optimizing search engine</h2>
                             <Link to="https://searchengineland.com/guide/what-is-seo" target="_blank" rel="noopener noreferrer" className='text-[20px]  max-sm:text-[15px] max-xs:text-[13px]   '>Learn More<i className="ri-arrow-right-up-line text-[25px] max-xs:text-[17px]  pl-1"></i></Link>
@@ -127,7 +128,7 @@ const Content = () => {
                             <img className='max-lg:h-[190px] max-small:h-[150px] max-lg:px-2  max-medium:ml-8  max-md:ml-3' src="https://zagfirst.com/wp-content/uploads/2018/06/search-engine-optimization.jpg" alt="" />
                         </div>
                     </div>
-                    <div className=" flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line1 right mb-14  max-xl:px-8 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 bg-black">
+                    <div className=" flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line1 right mb-14  max-xl:px-8 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 bg-black max-sm:hover:transform max-sm:hover:scale-105 max-sm:duration-500">
                         <div className=" elem-part1 h-full w-[50%] flex justify-between flex-col">
                             <h2 className=' bg-white w-[89%] text-[23px] font-semibold px-2 rounded-md max-lg:text-[20px]  max-sm:text-[17px]  max-small:text-[15px] max-xs:text-[13px] '>Social marketing media</h2>
                             <Link to="https://www.investopedia.com/terms/s/social-media-marketing-smm.asp" target="_blank" rel="noopener noreferrer" className='text-[20px]  max-sm:text-[15px] max-xs:text-[13px]  text-white'>Learn More<i className="ri-arrow-right-up-line text-[25px] max-xs:text-[17px]  pl-1"></i></Link>
@@ -136,7 +137,7 @@ const Content = () => {
                             <img className='max-lg:h-[190px] max-small:h-[120px] max-lg:px-2  max-medium:ml-8  max-md:ml-3  rounded-2xl' src="https://www.wordstream.com/wp-content/uploads/2022/02/social-media-marketing-fundamentals-wordstream.png" alt="" />
                         </div>
                     </div>
-                    <div className="  flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line2 left mb-14  max-xl:px-8 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 bg-black  max-medium:bg-white">
+                    <div className="  flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line2 left mb-14  max-xl:px-8 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 bg-black  max-medium:bg-white max-sm:hover:transform max-sm:hover:scale-105 max-sm:duration-500">
                         <div className="elem-part1 h-full w-[50%] flex justify-between flex-col">
                             <h2 className=' bg-white max-medium:bg-lime-300 w-[89%] text-[23px] font-semibold px-2 rounded-md max-lg:text-[20px]  max-sm:text-[17px]  max-small:text-[15px] max-xs:text-[13px]'>E-mail marketing services</h2>
                             <Link to="https://www.brevo.com/blog/what-is-email-marketing/" target="_blank" rel="noopener noreferrer" className='text-[20px]  max-sm:text-[15px] max-xs:text-[13px]  text-white max-medium:text-black'>Learn More<i className="ri-arrow-right-up-line text-[25px] max-xs:text-[17px]  pl-1"></i></Link>
@@ -145,7 +146,7 @@ const Content = () => {
                             <img className='max-lg:h-[190px] max-small:h-[120px] max-lg:px-2  max-medium:ml-8  max-md:ml-3  rounded-2xl' src="https://thebrandhopper.com/wp-content/uploads/2022/12/email-marketing-strategy.jpg" alt="" />
                         </div>
                     </div>
-                    <div className="  flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line2 right mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 max-medium:bg-black">
+                    <div className="  flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line2 right mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 max-medium:bg-black max-sm:hover:transform max-sm:hover:scale-105 max-sm:duration-500">
                         <div className="elem-part1 h-full w-[50%] flex justify-between flex-col">
                             <h2 className=' bg-lime-300 max-medium:bg-white w-[89%] text-[23px] font-semibold px-2 rounded-md max-lg:text-[20px]  max-sm:text-[17px]  max-small:text-[15px] max-xs:text-[13px]'>Youtube
                                 Management services</h2>
@@ -155,7 +156,7 @@ const Content = () => {
                             <img className='max-lg:h-[190px] max-small:h-[120px] max-lg:px-2  max-medium:ml-8  max-md:ml-3  rounded-2xl' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT4Bfvq6_6cF99FOjH0XJAzo_hT7P_RsXGd1weB_6WrA&s" alt="" />
                         </div>
                     </div>
-                    <div className=" flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line3 left mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 ">
+                    <div className=" flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line3 left mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 max-sm:hover:transform max-sm:hover:scale-105 max-sm:duration-500">
                         <div className="elem-part1 h-full w-[50%] flex justify-between flex-col">
                             <h2 className=' bg-lime-300  w-[89%] text-[23px] font-semibold px-2 rounded-md max-lg:text-[20px]  max-sm:text-[17px]  max-small:text-[15px] max-xs:text-[13px]'>Creative content
                                 Creation</h2>
@@ -165,7 +166,7 @@ const Content = () => {
                             <img className='max-lg:h-[190px] max-small:h-[120px] max-lg:px-2  max-medium:ml-8  max-md:ml-3  rounded-2xl' src="https://media.istockphoto.com/id/1679918797/vector/content-creation.jpg?s=612x612&w=0&k=20&c=Rwt1CiTLP3-kJwrVnmcbwYz-Q3Mava_4l2N_lOfiyPQ=" alt="" />
                         </div>
                     </div>
-                    <div className=" flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line3 right mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 bg-black">
+                    <div className=" flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line3 right mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 bg-black max-sm:hover:transform max-sm:hover:scale-105 max-sm:duration-500">
                         <div className="elem-part1 h-full w-[50%] flex justify-between flex-col">
                             <h2 className=' bg-white w-[89%] text-[23px] font-semibold px-2 rounded-md max-lg:text-[20px]  max-sm:text-[17px]  max-small:text-[15px] max-xs:text-[13px]'>Web
                                 development services</h2>
@@ -175,7 +176,7 @@ const Content = () => {
                             <img className='max-lg:h-[190px] max-small:h-[120px] max-lg:px-2  max-medium:ml-8  max-md:ml-3  rounded-2xl' src="https://p7.hiclipart.com/preview/578/658/575/web-development-web-developer-web-design-software-developer-web-design.jpg" alt="" />
                         </div>
                     </div>
-                    <div className="  flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line4 left mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 bg-black max-medium:bg-white">
+                    <div className="  flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line4 left mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 bg-black max-medium:bg-white max-sm:hover:transform max-sm:hover:scale-105 max-sm:duration-500">
                         <div className="elem-part1 h-full w-[50%] flex justify-between flex-col">
                             <h2 className=' bg-white max-medium:bg-lime-300  w-[89%] text-[23px] font-semibold px-2 rounded-md max-lg:text-[20px]  max-sm:text-[17px]  max-small:text-[15px] max-xs:text-[13px]'>Graphics &
                                 Video Editing</h2>
@@ -185,7 +186,7 @@ const Content = () => {
                             <img className='max-lg:h-[190px] max-small:h-[120px] max-lg:px-2  max-medium:ml-8  max-md:ml-3  rounded-2xl' src="https://png.pngtree.com/png-vector/20220524/ourmid/pngtree-video-editor-icon-design-png-image_4709312.png" alt="" />
                         </div>
                     </div>
-                    <div className=" flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line4 right mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 max-medium:bg-black">
+                    <div className=" flex-shrink-0 h-[330px] w-[47%] rounded-lg shadow-lg border border-solid p-14 flex  elem line4 right mb-14  max-xl:px-5 max-medium:w-[90%] max-medium:mx-auto  max-sm:h-[230px]  max-xs:h-[200px] max-xs:py-6  max-sm:py-8  max-small:w-[100%] max-small:px-2 max-sm:mb-9 max-medium:bg-black max-sm:hover:transform max-sm:hover:scale-105 max-sm:duration-500">
                         <div className="elem-part1 h-full w-[50%] flex justify-between flex-col">
                             <h2 className=' bg-lime-300 max-medium:bg-white  w-[89%] text-[23px] font-semibold px-2 rounded-md max-lg:text-[20px]  max-sm:text-[17px]  max-small:text-[15px] max-xs:text-[13px]'>Reputation  associate management</h2>
                             <Link to="https://blog.hubspot.com/marketing/reputation-management" target="_blank" rel="noopener noreferrer" className='text-[20px]  max-sm:text-[15px] max-xs:text-[13px] max-medium:text-white '>Learn More<i className="ri-arrow-right-up-line text-[25px] max-xs:text-[17px]  pl-1"></i></Link>
